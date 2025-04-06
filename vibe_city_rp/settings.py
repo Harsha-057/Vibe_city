41ce52b9aa09f0187ba8c31b9666c27f43e8e2bc
Harsha <hsambang@gitam.in>
2025-04-06 11:25:42 +0530

    changes

--- vibe_city_rp/settings.py
+++ vibe_city_rp/settings.py
@@ -1,5 +1,7 @@
 import os
+import dj_database_url
 from pathlib import Path
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -72,11 +74,15 @@ WSGI_APPLICATION = 'vibe_city_rp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME':  BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # Authentication
