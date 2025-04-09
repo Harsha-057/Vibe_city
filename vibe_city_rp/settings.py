9cc30488b231abf502dd11df1fda0235077cf2b6
bobby242fb <122474054+bobby242fb@users.noreply.github.com>
2025-04-09 19:16:50 +0100

    added

--- vibe_city_rp/settings.py
+++ vibe_city_rp/settings.py
@@ -141,9 +141,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
